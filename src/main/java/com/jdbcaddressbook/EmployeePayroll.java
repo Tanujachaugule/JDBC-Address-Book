3116beef2575a2d371894a2e5ff8e497c6c59e34
Tanujachaugule <tanujachaugule1999@gmail.com>
2023-03-29 10:06:44 +0530

    Added: Employee Payrollservice to retrive the data

--- src/main/java/com/jdbcaddressbook/EmployeePayroll.java
+++ src/main/java/com/jdbcaddressbook/EmployeePayroll.java
@@ -3,17 +3,19 @@ package com.jdbcaddressbook;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 public class EmployeePayroll {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         JDBConnection con = new JDBConnection();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int choice = -1;
-        while (choice!=0){
+        while (choice != 0) {
             System.out.println("Enter your choice:\n1. DISPLAY EMPLOYEE PAYROLL\n0. EXIT");
             choice = Integer.parseInt(br.readLine());
-            switch (choice){
-                case 1: con.display();
+            switch (choice) {
+                case 1:
+                    con.display();
                     break;
-                case 0: choice=0;
+                case 0:
+                    choice = 0;
                     con.close();
             }
         }
--- src/main/java/com/jdbcaddressbook/JDBConnection.java
+++ src/main/java/com/jdbcaddressbook/JDBConnection.java
@@ -34,9 +34,4 @@ public class JDBConnection {
         con.close();
         System.out.println("Connection closed.....");
     }
-
-    public double getSalaryFromJDBC(String tanuja) {
-        return 0;
-    }
 }
-
